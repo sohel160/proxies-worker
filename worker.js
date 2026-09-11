18172847d7c754396156f2adb54517911853c816
sohel160 <sohelcmc53rd@gmail.com>
2026-09-11 17:34:52 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -31,23 +31,23 @@ export default {
 
       const proxies = `
 proxies:
-  - { name: "HTTP-01", type: http, server: 113.212.109.164, port: 22622 }
-  - { name: "HTTP-02", type: http, server: 113.212.109.163, port: 22622 }
-  - { name: "HTTP-03", type: http, server: 113.212.109.132, port: 22622 }
-  - { name: "HTTP-04", type: http, server: 103.84.36.225, port: 52148 }
-  - { name: "HTTP-05", type: http, server: 103.84.36.237, port: 22622 }
-  - { name: "HTTP-06", type: http, server: 113.212.109.188, port: 22622 }
-  - { name: "HTTP-07", type: http, server: 103.84.36.237, port: 22622 }
-  - { name: "HTTP-08", type: http, server: 113.212.109.56, port: 22622 }
-  - { name: "HTTP-09", type: http, server: 103.84.38.110, port: 22622 }
-  - { name: "HTTP-10", type: http, server: 103.84.38.84, port: 22622 }
-  - { name: "HTTP-11", type: http, server: 103.84.38.52, port: 22622 }
-  - { name: "HTTP-12", type: http, server: 103.84.37.196, port: 22622 }
-  - { name: "HTTP-13", type: http, server: 103.84.37.132, port: 22622 }
-  - { name: "HTTP-14", type: http, server: 103.84.37.123, port: 22622 }
-  - { name: "HTTP-15", type: http, server: 103.84.37.100, port: 22622 }
-  - { name: "HTTP-16", type: http, server: 103.84.37.72, port: 22622 }
-  - { name: "HTTP-17", type: http, server: 103.84.36.237, port: 22622 }
+  - { name: "HTTP-01", type: http, server: 113.212.109.164, port:  }
+  - { name: "HTTP-02", type: http, server: 113.212.109.163, port:  }
+  - { name: "HTTP-03", type: http, server: 113.212.109.132, port:  }
+  - { name: "HTTP-04", type: http, server: 103.84.36.225, port:  }
+  - { name: "HTTP-05", type: http, server: 103.84.36.237, port:  }
+  - { name: "HTTP-06", type: http, server: 113.212.109.188, port:  }
+  - { name: "HTTP-07", type: http, server: 103.84.36.237, port:  }
+  - { name: "HTTP-08", type: http, server: 113.212.109.56, port:  }
+  - { name: "HTTP-09", type: http, server: 103.84.38.110, port:  }
+  - { name: "HTTP-10", type: http, server: 103.84.38.84, port:  }
+  - { name: "HTTP-11", type: http, server: 103.84.38.52, port:  }
+  - { name: "HTTP-12", type: http, server: 103.84.37.196, port:  }
+  - { name: "HTTP-13", type: http, server: 103.84.37.132, port:  }
+  - { name: "HTTP-14", type: http, server: 103.84.37.123, port:  }
+  - { name: "HTTP-15", type: http, server: 103.84.37.100, port:  }
+  - { name: "HTTP-16", type: http, server: 103.84.37.72, port:   }
+  - { name: "HTTP-17", type: http, server: 103.84.36.237, port:  }
 `
 
       return new Response(proxies, {
